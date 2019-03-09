e6d8e3410c1ee27667ee60acb6e50b3937599c36
Noelle Legrain <noelle.legrain@gmail.com>
2019-03-09 13:28:16 +0000

    Section 2, type coercion and variable mutation

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,21 @@
-console.log("Hello World!");
+var firstName = 'John';
+console.log(firstName);
+
+// Type Coercion
+var age = 28;
+console.log(firstName + ' ' + age)
+
+var job, isMarried;
+job = 'teacher';
+//isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// Variable mutation
+age = 'twenty eight';
+job = 'driver';
+
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+lastName = prompt('What is his last name?');
+console.log(firstName + ' ' + lastName);
