fcfbae9f08ae8c9ac7ec8b323b347676f319af7f
Noelle Legrain <noelle.legrain@gmail.com>
2019-04-11 16:06:30 +0100

    zero out score when player hits 1

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -14,17 +14,10 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-
-
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-//var x = document.querySelector('#score-0').textContent;
-//console.log(x);
-
 document.querySelector('.dice').style.display = 'none';
 
 document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0;
+document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
 document.getElementById('current-1').textContent = '0';
 
@@ -39,7 +32,24 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //update the round score if the rolled number was not a 1
+    if (dice !== 1) {
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+       activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+       roundScore = 0;
 
+       document.getElementById('current-0').textContent = '0';
+       document.getElementById('current-1').textContent = '0';
 
+       document.querySelector('.player-0-panel').classList.toggle('active');
+       document.querySelector('.player-1-panel').classList.toggle('active');
 
+    }
 });
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
