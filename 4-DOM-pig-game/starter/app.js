1de990c5551a28abf2ad67cd8db9b45a35912c2a
Noelle Legrain <noelle.legrain@gmail.com>
2019-05-07 08:35:51 +0100

    make styling change upon win

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -49,9 +49,16 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
     //Check if player won the game
-    nextPlayer();
+    if (scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = "Winner!";
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    } else {
+        nextPlayer();
 
-});
+    };
+   });
 
 function nextPlayer() {
        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
