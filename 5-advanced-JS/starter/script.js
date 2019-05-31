337a46181594e3399abe84e57b82b0a17564268b
Noelle Legrain <noelle.legrain@gmail.com>
2019-05-31 13:47:27 +0100

    lesson 5, closures

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -48,7 +48,7 @@ var teacherQuestion = interviewQuestions('teacher');
 teacherQuestion('John');
 
 interviewQuestions('teacher')('Mark');
-*/
+
 
 // Lecture IIFE
 
@@ -61,3 +61,35 @@ interviewQuestions('teacher')('Mark');
   var score = Math.random() * 18;
   console.log(score >= 5 - goodluck);
 })(5);
+
+
+//Closures
+
+function retirement(retirementAge) {
+  var statement = ' years until retirement.';
+  return function(yearOfBirth) {
+    var age = 2018 - yearOfBirth;
+    console.log((retirementAge - age) + statement);
+  }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1980);
+
+retirement(66)(1980);
+console.log(a);
+*/
+
+function interviewQuestions(job) {
+  return function(name) {
+    if (job === 'designer') {
+        console.log(name + ' , can you please explainwhat UX design is?');
+    } else if (job === 'teacher') {
+      console.log('What subject do you teach, ' + name +'?');
+    } else {
+      console.log('Hello ' + name + ', what do you do?');
+    }
+  }
+}
+
+interviewQuestions('teacher')('John');
