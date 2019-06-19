d12ae2138a9a419498b90f47f01a3ae95f931de8
Noelle Legrain <noelle.legrain@gmail.com>
2019-06-19 21:31:11 +0100

    create init function to set up event listeners

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -5,31 +5,62 @@ var budgetController = (function() {
 })();
 
 //ui controller
-var userInterfaceController = (function() {
+var UIController = (function() {
 
+  var DOMStrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  };
+
+  return {
+    getInput: function() {
+
+      return {
+        type: document.querySelector(DOMStrings.inputType).value,
+        description: document.querySelector(DOMStrings.inputDescription).value,
+        value: document.querySelector(DOMStrings.inputValue).value,
+      }
+    },
+
+    getDOMStrings: function() {
+      return DOMStrings;
+    }
+  };
 
 })();
 
 //global app controller
 var controller = (function(budgetCtrl, UIContrl) {
 
-  var ctrlAddItem = function() {
-    console.log('it works');
-  }
+  var setupEventListeners = function() {
+    var DOM = UIContrl.getDOMStrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keypress', function(event) {
+       if (event.charCode === 13) {
+         ctrlAddItem();
+       }
+    });
+  };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  var ctrlAddItem = function() {
+    var input = UIContrl.getInput();
+    console.log(input);
+  }
     //get input data
     //add item to budget budget budgetController
     //add new item to user user interface
     //calculate budget
     //display budget
 
+    return {
+      init: function() {
+        console.log('Application has started.');
+        setupEventListeners();
+      }
+    };
 
-  document.addEventListener('keypress', function(event) {
-     if (event.charCode === 13) {
-       ctrlAddItem();
-     }
-  });
+})(budgetController, UIController);
 
-
-})(budgetController, userInterfaceController);
+controller.init();
